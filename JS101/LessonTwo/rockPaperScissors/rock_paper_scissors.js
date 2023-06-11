90744860703e3d049faf1845af0e41054653811c
zijin1994 <zijin@ualberta.ca>
2023-06-11 03:48:49 +0000

    refactor rps game

--- JS101/LessonTwo/rockPaperScissors/rock_paper_scissors.js
+++ JS101/LessonTwo/rockPaperScissors/rock_paper_scissors.js
@@ -1,9 +1,6 @@
 const { question } = require("readline-sync");
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-//Put all of the choices in an object as keys. Then for each choice,
-//put the ones that it can beat in an array as value.
-//We could also improve the performance by using set for faster look up time,
-//but here we only have two values so it does not matter too much.
+const [YOU_WON, COMPUTER_WON, TIE] = [1, 2, 0];
 const BEAT = {
   rock: ['lizard', 'scissors'],
   paper: ['rock', 'spock'],
@@ -11,158 +8,230 @@ const BEAT = {
   lizard: ['paper', 'spock'],
   spock: ['rock', 'scissors']
 };
-//Initialize match score. Index 0 is user score, index 1 is computer score.
-const MATCH_SCORE = [0, 0];
 
-//check if there is a winner.
-function hasGrandWinner() {
-  return (MATCH_SCORE[0] === 3 || MATCH_SCORE[1] === 3);
+const WINNING_SCORE = 3;
+
+function lowerCaseQuestion() {
+  let result = question().toLowerCase();
+  return result;
 }
 
-function printGrandWinner() {
-  if (MATCH_SCORE[0] === 3) {
-    console.log(`Congraduations! You are the grand winner!\nThe final score is: ${MATCH_SCORE[0]} : ${MATCH_SCORE[1]}`);
-  } else {
-    console.log(`Sorry! Computer is the grand winner!\nThe final score is: ${MATCH_SCORE[0]} : ${MATCH_SCORE[1]}`);
+function hasGrandWinner(userScore, computerScore) {
+  return (userScore === WINNING_SCORE || computerScore === WINNING_SCORE);
+}
+
+function printGrandWinner(userScore, computerScore) {
+  console.log("********************");
+  if (userScore === WINNING_SCORE) {
+    console.log(`Congraduations! You are the grand winner!\nThe final score is: ${userScore} : ${computerScore}`);
+  }
+  else {
+    console.log(`Sorry! Computer is the grand winner!\nThe final score is: ${userScore} : ${computerScore}`);
   }
 }
 
 function resetScore() {
-  MATCH_SCORE[0] = 0;
-  MATCH_SCORE[1] = 0;
+  return [0, 0]
 }
 
-function displayScore() {
-  console.log(`Current score is: User: ${MATCH_SCORE[0]}, Computer: ${MATCH_SCORE[1]}`);
+function displayScore(userScore, computerScore) {
+  console.log(`Current score is: User: ${userScore}, Computer: ${computerScore}`);
 }
 
-//add point to user/computer.
-function addPoint(choice, computerChoice) {
-  if (userWins(choice, computerChoice)) {
-    MATCH_SCORE[0] += 1;
-  } else if (isDraw(choice, computerChoice)) {
-    // does nothing.
-  } else {
-    MATCH_SCORE[1] += 1;
+function addPoint(userScore, computerScore, result) {
+  if (result === YOU_WON) {
+    userScore += 1;
   }
+  else if (result === COMPUTER_WON) {
+    computerScore += 1;
+  }
+  return [userScore, computerScore];
 }
 
-//check if input is valid, including one letter input.
+function getUserChoice() {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = lowerCaseQuestion();
+  return choice;
+}
+
+function isOneLetterInput(choice) {
+  return choice.length === 1;
+}
+
+function referToMany(choices) {
+  return choices.length > 1;
+}
+
+function validateUserChoice(choice) {
+  while (!isValidInput(choice)) {
+    prompt("That's not a valid choice");
+    choice = lowerCaseQuestion();
+  }
+
+  if (isOneLetterInput(choice)) {
+    let choices = toFullLengthInput(choice); // if 's' was the input, this array would contain two values.
+
+    if (referToMany(choices)) { // handle 's' input
+      choice = sInputHandler();
+    }
+    else { // otherwise, there is only one value in choices array, simply assign it to choice.
+      choice = choices[0];
+    }
+  }
+  return choice;
+}
+
 function isValidInput(choice) {
   if (VALID_CHOICES.includes(choice)) {
     return true;
   }
-  // check for one letter input.
+
   let validChoices = VALID_CHOICES.filter(element => element[0] === choice);
   return validChoices.length > 0;
 }
 
-//convert one letter input to full length input.
 function toFullLengthInput(choice) {
   let choices = VALID_CHOICES.filter(element => element[0] === choice);
   return choices;
 }
 
-//validate 's' input, also handles the case where user inputs 1 or 2.
 function isSpockOrScissors(choice) {
   return ['scissors', 'spock', '1', '2'].includes(choice);
 }
 
-//handle 's' input case where user enters 1 or 2 instead of full name of choice.
+function validateSpockOrScissors(choice) {
+  while (!isSpockOrScissors(choice)) { // check if user input 1 or 2 or the full name of choice.
+    prompt("That's not a valid choice");
+    choice = lowerCaseQuestion();
+  }
+  return choice;
+}
+
 function numberInputHandler(choice) {
   if (choice === '1') {
     return 'scissors';
-  } else {
+  }
+  else {
     return 'spock';
   }
 }
 
-//check if user wins.
+//handle s input.
+function sInputHandler() {
+  prompt("There are two choices starting with letter 's', pick one: 1) scissors 2) spock");
+  let choice = lowerCaseQuestion();
+  choice = validateSpockOrScissors(choice);
+
+  if (choice === '1' || choice === '2') {
+    choice = numberInputHandler(choice);
+  }
+  return choice;
+}
+
+function getComputerChoice() {
+  let randomIndex = Math.ceil(Math.random() * VALID_CHOICES.length) - 1;
+  return VALID_CHOICES[randomIndex];
+}
+
 function userWins(choice, computerChoice) {
   // access the values of what user's choice can beat in BEAT object
   // check if it includes computer's choice
   return BEAT[choice].includes(computerChoice);
 }
 
-//check if it is draw.
 function isDraw(choice, computerChoice) {
   return choice === computerChoice;
 }
 
-function displayWinner(choice, computerChoice) {
+function matchResult(choice, computerChoice) {
+  if (isDraw(choice, computerChoice)) {
+    return TIE;
+  }
+  else if (userWins(choice, computerChoice)) {
+    return YOU_WON;
+  }
+  else {
+    return COMPUTER_WON;
+  }
+}
+
+function displayWinner(choice, computerChoice, result) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-  if (isDraw(choice, computerChoice)) { //checking this equal condition first so we don't have to write anything for else condition, which is just all losing conditions.
+  if (result === TIE) {
     console.log("It is a tie!");
-  } else if (userWins(choice, computerChoice)) {
+  }
+  else if (result === YOU_WON) {
     console.log("You win!");
-  } else {
+  }
+  else {
     console.log("Computer wins!");
   }
+}
+
+function anotherGame() {
+  prompt("Do you want to play again (y/n)?");
+  let answer = lowerCaseQuestion();
+
+  answer = validateAnotherGameAnswer(answer);
+  if (answer === 'y') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateAnotherGameAnswer(answer) {
+  while (!isValidAnotherGameAnswer(answer)) {
+    prompt("Please enter 'y' or 'n'.");
+    answer = lowerCaseQuestion();
+  }
+  return answer;
+}
+
+function isValidAnotherGameAnswer(answer) {
+  return answer === 'n' || answer === 'y';
 }
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-//While loop to keep playing without using break.
+function welcomeMessage() {
+  console.log("**********Welcome to rock paper scissors lizard spock game!**********");
+}
+
+function clearScreen() {
+  console.clear();
+}
+
+clearScreen();
+welcomeMessage();
+
+let [userScore, computerScore] = resetScore();
 let playAgain = true;
-while (playAgain && !hasGrandWinner()) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = question();
 
-  while (!isValidInput(choice)) {
-    prompt("That's not a valid choice");
-    choice = question();
-  }
+while (playAgain && !hasGrandWinner(userScore, computerScore)) {
 
-  if (choice.length === 1) {
-    let choices = toFullLengthInput(choice); // if 's' was the input, this array would contain two values.
+  let choice = getUserChoice();
+  choice = validateUserChoice(choice);
 
-    if (choices.length > 1) { // handle 's' input
-      prompt("There are two choices starting with letter 's', pick one: 1) scissors 2) spock");
-      choice = question();
+  let computerChoice = getComputerChoice();
+  let result = matchResult(choice, computerChoice);
+  clearScreen();
 
-      while (!isSpockOrScissors(choice)) { // check if user input 1 or 2 or the full name of choice.
-        prompt("That's not a valid choice");
-        choice = question();
-      }
+  displayWinner(choice, computerChoice, result);
 
-      if (choice === '1' || choice === '2') { // if user types 1 or 2 instead of the full name of choice.
-        choice = numberInputHandler(choice);
-      }
-    } else { // otherwise, there is only one value in choices array, simply assign it to choice.
-      choice = choices[0];
-    }
+  [userScore, computerScore] = addPoint(userScore, computerScore, result);
+  displayScore(userScore, computerScore);
 
-  }
+  if (hasGrandWinner(userScore, computerScore)) {
+    printGrandWinner(userScore, computerScore);
 
-  let randomIndex = Math.ceil(Math.random() * VALID_CHOICES.length) - 1;
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  displayWinner(choice, computerChoice);
-  addPoint(choice, computerChoice);
-  displayScore();
-
-  if (hasGrandWinner()) {
-    printGrandWinner();
-
-    prompt("Do you want to play again (y/n)?");
-    let answer = question().toLowerCase();
-
-    while (answer[0] !== 'n' && answer[0] !== 'y') {
-      prompt("Please enter 'y' or 'n'.");
-      answer = question().toLowerCase();
-
-    }
-
-    if (answer[0] !== 'y') {
-      playAgain = false;
-    }
+    playAgain = anotherGame();
 
     //reset score and clear the console.
-    resetScore();
-    console.clear();
+    [userScore, computerScore] = resetScore();
+    clearScreen();
   }
-
 }
